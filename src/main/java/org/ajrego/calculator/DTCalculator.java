ede2967b4ea21a554fe7975f400c5971eb002ac1
AJRego <rego.andre99@gmail.com>
2021-04-29 11:01:54 -0500

    Implements dragged scene

--- src/main/java/org/ajrego/calculator/DTCalculator.java
+++ src/main/java/org/ajrego/calculator/DTCalculator.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.ajrego.util.DraggedScene;
 
-public class DTCalculator extends Application {
+public class DTCalculator extends Application implements DraggedScene {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        this.onDraggedScene(root);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
